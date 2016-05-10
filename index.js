4770d9ae4d56c2f543d5be1d103258cab33ec993
paul <paul@wizmacau.com>
2016-05-10 15:35:39 +0800

    update

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 var express = require('express');
-var ParseDashboard = require('parse-dashboard');
 var ParseServer = require('parse-server').ParseServer;
 
 var app = express();
@@ -9,35 +8,12 @@ var api = new ParseServer({
     cloud: '/parse/cloud/main.js', // Absolute path to your Cloud Code
     appId: process.env.APP_ID,
     masterKey: process.env.MASTER_KEY, // Keep this key secret!
-    serverURL: 'http://localhost:' + process.env.PARSE_SERVER_PORT + '/parse' // Don't forget to change to https if needed
+    serverURL: 'http://localhost:1337/parse' // Don't forget to change to https if needed
 });
 
 // Serve the Parse API on the /parse URL prefix
 app.use('/parse', api);
-// var allowInsecureHTTP =true;
-var dashboard = new ParseDashboard({
-    "apps": [
-        {
-            "serverURL": "http://" + process.env.PARSE_PUBLIC_ADDR + ":" + process.env.PARSE_PUBLIC_PORT + "/parse",
-            "appId": process.env.APP_ID,
-            "masterKey": process.env.MASTER_KEY, // Keep this key secret!
-            "appName": "demo"
-        }
-    ],
-    "users": [
-        {
-            "user": process.env.ADMIN_USER,
-            "pass": process.env.ADMIN_PASS
-        }
-    ]
-}, process.env.PARSE_DASHBOARD_ALLOW_INSECURE_HTTP);
 
-// make the Parse Dashboard available at /dashboard
-app.use('/dashboard', dashboard);
-
-var httpServer = require('http').createServer(app);
-httpServer.listen(4040);
-
-app.listen(process.env.PARSE_SERVER_PORT, function () {
-    console.log('parse-server-example running on port 1337.');
+app.listen(1337, function () {
+    console.log('parse-server running on port 1337.');
 });
